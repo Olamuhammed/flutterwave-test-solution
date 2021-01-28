640b8357ed76d47874eac961b35be6b8a5a83a59
Olagunju Mohammed <damolagunju@gmail.com>
2021-01-29 00:24:30 +0100

    fix: fixed main file path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const routes = require('./routes')
+const routes = require('./src/routes')
 const app = express()
 
 app.use(cors())
